cd4e0ba07ff86ae89a1cff0a4e950cee96e646c2
cy19960119 <CYcz1996>
2018-02-28 09:23:45 +0800

    add views

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,74 @@
-//index.js
-//获取应用实例
-const app = getApp()
 
+// 引入coolsite360交互配置设定
+require('coolsite.config.js');
+
+// 获取全局应用程序实例对象
+var app = getApp();
+
+// 创建页面实例对象
 Page({
+  /**
+   * 页面名称
+   */
+  name: "index",
+  /**
+   * 页面的初始数据
+   */
+
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  
+
+  
+
+  
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad () {
+    // 注册coolsite360交互模块
+    app.coolsite360.register(this);
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady () {
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow () {
+    // 执行coolsite360交互组件展示
+    app.coolsite360.onShow(this);
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh () {
+    
+  },
+
+
+  //以下为自定义点击事件
+  
 })
+
