efdfb3f659540d59b93d8813bac5f5727494ca58
TimBoys <786575400@qq.com>
2018-03-22 11:25:50 +0800

    自提点删除/修改

--- pages/personal/address/addAddress/addAddress.js
+++ pages/personal/address/addAddress/addAddress.js
@@ -7,16 +7,24 @@ Page({
    * 页面的初始数据
    */
   data: {
+    addrDetail:"",
     addrName:"地图定位",
     longitude:"",
-    latitude:""
+    latitude:"",
+    id:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if(options.id){
+      this.setData({
+        addrDetail:options.detail,
+        addrName:options.name,
+        id:options.id
+      })
+    }
   },
 
   /**
@@ -69,15 +77,41 @@ Page({
   },
   // 保存信息
   formSubmit: function (e) {
-    if(e.detail.value.detail){
     var data = {
+      id: this.data.id,
       userId: wx.getStorageSync("userId"),
       name: this.data.addrName,
       detail: e.detail.value.detail,
       longitude: this.data.longitude,
       latitude: this.data.latitude
+    };
+    //修改信息
+    if (e.detail.value.detail && this.data.id){
+      wx.request({
+        url: app.globalData.domain +'/userAddress/updateById',
+        method: "POST",
+        header: {
+          "content-type": "application/json"
+        },
+        data: data,
+        success:function(res){
+          var data = res.data
+          if (data.errorCode == 0) {
+            console.log('修改地址成功');
+            wx.showToast({
+              title: '保存成功',
+              success: function () {
+                wx.navigateBack({
+                  delta: 1
+                })
+              }
+            })
+          } 
+        }
+      })
     }
-    console.log(data);
+    // 新增信息
+    else if(e.detail.value.detail){
     wx.request({
       url: app.globalData.domain + '/userAddress/insert',
       method: 'POST',
@@ -86,10 +120,8 @@ Page({
       },
       data: data,
       success: function (res) {
-        console.log(res);
         var data = res.data
         if (data.errorCode == 0) {
-          console.log('插入地址成功');
           wx.showToast({
             title: '保存成功',
             success: function(){
@@ -101,7 +133,6 @@ Page({
         }
       }
     })
-
     }else{
       wx.showModal({
         title: '请输入自提详细地址',
@@ -115,15 +146,46 @@ Page({
     wx.chooseLocation({
       type:"wgs84",
       success:function(res){
-        console.log(res);
         self.setData({
           addrName:res.name,
           longitude:res.longitude,
           latitude:res.latitude
           })
-      },
-      fail:function(err){
-        console.log(err);
+      }
+    })
+  },
+  // 删除自提点
+  deleAddress:function(e){
+    console.log(this)
+    var _this = this;
+    wx.showModal({
+      title: '是否确认删除此自提点？',
+      success: function (res) {
+      if (res.confirm) {
+      if (_this.data.id){
+      wx.request({
+        url: app.globalData.domain + '/userAddress/deleteById',
+        data:{
+          id: _this.data.id
+        },
+        success:function(res){
+          console.log(res);
+          if(res.data.errorCode == 0){
+            wx.showToast({
+              title: '删除成功',
+              success:function(){
+                wx.navigateBack({
+                  delta: 1
+                })
+              }
+            })
+          }
+        }
+      })
+    }
+    }else{
+       return;
+        }
       }
     })
   }  
