b5c12ea3b57b9cc338fa0cf381c575793b27f2f3
DioZhou <779080947@qq.com>
2018-04-08 17:22:44 +0800

    新增系统通知页面

---
+++ pages/comments/systemNotice/systemNotice.js
@@ -0,0 +1,81 @@
+// pages/comments/systemNotice/systemNotice.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    noticeList:[{
+      time: "2018/03/08 09:07",
+      title: "恭候女王驾到",
+      description: "女王更优惠",
+      imgUrl:"../../../images/addnum.png"
+    }, {
+      time: "2018/02/17 19:33",
+      title: "迪欧大魔王",
+      description: "天下第一帅",
+      imgUrl: "../../../images/minusnum.png"
+    }, {
+      time: "2018/02/05 13:42",
+      title: "白堤",
+      description: "西湖美景，三月天内",
+      imgUrl: "../../../images/addnum.png"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
