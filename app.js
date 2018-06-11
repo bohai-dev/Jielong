78b81185269c0d218097010c2093bfc3942ffe8b
TimBoys <786575400@qq.com>
2018-06-11 13:35:20 +0800

    oss域名更换

--- app.js
+++ app.js
@@ -191,9 +191,8 @@ App({
   //全局数据
   globalData: {
     userInfo: null,
-    // domain: "https://www.95cfun.com",
     domain: "https://www.95cfuns.com",
-    domainUpload: "https://upload.95cfun.com/",
+    domainUpload: "https://upload.95cfuns.com/",
     xOssProcess:"?x-oss-process=image/quality,q_60",    
   //  domain: "http://47.100.12.188:8081",
     // domain: "http://192.168.1.108:8081",
